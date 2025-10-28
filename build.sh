1d3f65103993904dba1fe33ec1c927ae57991e1f
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-10-27 21:36:50 -0500

    :pick: Temporary `build.sh` script to keep me going while I `Makefile`

---
+++ build.sh
@@ -0,0 +1,1 @@
+/c/cygwin64/bin/gcc.exe -Wall -Wextra -Wpedantic -pedantic-errors -fsanitize=undefined -fsanitize-trap -fsanitize=enum -fsanitize=bool -fsanitize=bounds -fanalyzer -std=c99 -D_POSIX_C_SOURCE=200809L -Og -g3 -o tcp_server.exe tcp_server.c

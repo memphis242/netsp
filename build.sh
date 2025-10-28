9e7342c438b781020293c8bd46bf09386fd9b7d9
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-10-27 21:38:48 -0500

    :pick: `build.sh` Prepare for `tcp_client` as well

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 /c/cygwin64/bin/gcc.exe -Wall -Wextra -Wpedantic -pedantic-errors -fsanitize=undefined -fsanitize-trap -fsanitize=enum -fsanitize=bool -fsanitize=bounds -fanalyzer -std=c99 -D_POSIX_C_SOURCE=200809L -Og -g3 -o tcp_server.exe tcp_server.c
+
+# /c/cygwin64/bin/gcc.exe -Wall -Wextra -Wpedantic -pedantic-errors -fsanitize=undefined -fsanitize-trap -fsanitize=enum -fsanitize=bool -fsanitize=bounds -fanalyzer -std=c99 -D_POSIX_C_SOURCE=200809L -Og -g3 -o tcp_client.exe tcp_client.c

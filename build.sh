253cb47fe46264549cb74889f25f67263164f8a8
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-11-06 08:17:46 -0600

    :pick: `build.sh` Added build cmd for Linux

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-/c/cygwin64/bin/gcc.exe -Wall -Wextra -Wpedantic -pedantic-errors -fsanitize=undefined -fsanitize-trap -fsanitize=enum -fsanitize=bool -fsanitize=bounds -fanalyzer -std=c99 -D_POSIX_C_SOURCE=200809L -Og -g3 -o tcp_server.exe tcp_server.c
+set -x
+
+# /c/cygwin64/bin/gcc.exe -Wall -Wextra -Wpedantic -pedantic-errors -fsanitize=undefined -fsanitize-trap -fsanitize=enum -fsanitize=bool -fsanitize=bounds -fanalyzer -std=c99 -D_POSIX_C_SOURCE=200809L -Og -g3 -o tcp_server.exe tcp_server.c
+gcc -Wall -Wextra -Wpedantic -pedantic-errors -fsanitize=undefined -fsanitize-trap -fsanitize=enum -fsanitize=bool -fsanitize=bounds -fsanitize=address -fanalyzer -std=c99 -D_POSIX_C_SOURCE=200809L -Og -g3 -o build/tcp_server tcp_server.c
 
 # /c/cygwin64/bin/gcc.exe -Wall -Wextra -Wpedantic -pedantic-errors -fsanitize=undefined -fsanitize-trap -fsanitize=enum -fsanitize=bool -fsanitize=bounds -fanalyzer -std=c99 -D_POSIX_C_SOURCE=200809L -Og -g3 -o tcp_client.exe tcp_client.c

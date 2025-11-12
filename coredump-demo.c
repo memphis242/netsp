781f232d513844a99f795326a019f0726ea38aa3
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-11-12 09:06:32 -0600

    :brain: Demo to check out a coredump /w gdb
    Quick Ref:
    - On many Linux distros, core dumps are disabled by default. Check /w
      `ulimit -c` and enable with `ulimit -c unlimited`
    - On Fedora, which is a `systemd`-based system (like Debian and Ubuntu
      as well), core dumps are captured and managed by the
      `systemd-coredump` service
       - To see coredumps captured by `systemd-coredump`, use `coredumpctl
         list`
       - To extract a coredump, `coredumpctl dump <pid> >
         /path/to/dir/core.<name>`
    - To inspect a coredump /w gdb, `gdb <program> <coredump file>`
    - `bt full` to see all stack frames
    - `frame X`, where X is frame number, to view that stack frame and
      "check out" that context
    - `info locals` to print the local context
    - `info registers` to print CPU registers

---
+++ coredump-demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int user_input = 0;
+   printf("User input: ");
+   scanf("%d", user_input);
+   printf("User input received: ", user_input);
+
+   return 0;
+}
